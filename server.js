6311d4839bd3585869e36339f44e0a02e2e7dbd2
Eduardo <95320384+EduardoTr01@users.noreply.github.com>
2024-11-07 11:11:45 -0300

    Configurar CORS y permitir conexiones externas

--- server.js
+++ server.js
@@ -3,17 +3,23 @@ const mysql = require('mysql');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 // Cargar variables de entorno desde .env
 dotenv.config();
 
-app.use(cors());
+// Habilitar CORS para todos los orígenes
+app.use(cors({
+  origin: '*'
+}));
+
+// Middleware para parsear JSON
+app.use(express.json());
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST || '127.0.0.1',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD ||'',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'vehiculo'
 });
 
@@ -25,6 +31,7 @@ connection.connect((err) => {
   console.log('Conectado a la base de datos MySQL');
 });
 
+// Ruta para obtener datos de la base de datos
 app.get('/opciones', (req, res) => {
   connection.query('SELECT patente, modelo, version, anio, kilometros, color, precioLista, precioMedio, precioMinimo FROM usado', (error, results) => {
     if (error) throw error;
@@ -32,6 +39,7 @@ app.get('/opciones', (req, res) => {
   });
 });
 
-app.listen(port, () => {
+// Iniciar el servidor y permitir conexiones externas
+app.listen(port, '0.0.0.0', () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
